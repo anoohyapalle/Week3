e00eb7f8c7b790849477c362ee8228f7d9b75359
anoohyapalle <anoohyapalle@gmail.com>
2024-08-14 14:47:31 +0530

    Commiting Exercise 1 of Week 3

--- payroll_calculator.py
+++ payroll_calculator.py
@@ -4,4 +4,4 @@ def calculate_pay(hours, rate):
     pay = hours * rate
     return pay
 total_pay = calculate_pay(number_of_hours, hourly_rate)
-print("The total pay for the employee is: Rs.", total_pay)
+print("The total pay for the employee is Rs.", total_pay)
